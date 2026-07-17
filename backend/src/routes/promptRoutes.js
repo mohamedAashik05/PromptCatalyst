efe21ee499071cf5fa2e59a3dafc45e04500fa1f
Mohamed Aashik I <aashik.i2005@gmail.com>
2026-07-17 20:51:21 +0530

    Delete backend directory

--- backend/src/routes/promptRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { optimizePrompt } = require("../controllers/promptController");
-
-router.post("/optimize", optimizePrompt);
-
-module.exports = router;
